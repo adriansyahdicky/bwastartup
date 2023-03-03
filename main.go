dbe0338b0ed9af7ce553fb048f6dec4a9d9f50e2
adriansyahdickyklickers <mochammad_dicky@klickersgroup.com>
2023-03-03 18:39:13 +0700

    Init project Register user endpoint

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=postgres password=root dbname=bwstartup port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection Database Success")
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
