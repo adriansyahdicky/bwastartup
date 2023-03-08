440b0677a2c9df8e68b1b84755b43009ee8f5173
adriansyahdickyklickers <mochammad_dicky@klickersgroup.com>
2023-03-08 10:37:54 +0700

    auth middleware

--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(ID int, fileLocation string) (User, error)
+	GetUserById(ID int) (User, error)
 }
 
 type service struct {
@@ -92,4 +93,17 @@ func (s *service) SaveAvatar(ID int, fileLocation string) (User, error) {
 	}
 
 	return updateUser, nil
+}
+
+func (s *service) GetUserById(ID int) (User, error) {
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, nil
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("No user found with that ID")
+	}
+
+	return user, nil
 }
