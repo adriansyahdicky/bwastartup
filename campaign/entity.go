9861f431db58deaf999d28c49425a166c4f7b1aa
adriansyahdickyklickers <mochammad_dicky@klickersgroup.com>
2023-03-08 10:49:55 +0700

    campaign entity

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,21 @@
 package campaign
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+}
